eb6b794641229338108bb74b5b009819312659f3
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2022-09-17 20:07:01 +0200

    Delete demo directory

--- demo/displayusers.py
+++
@@ -1,7 +0,0 @@
-
-from  filehandler import getallusers
-def displayallusers():
-    print("------------- Users list -------------------")
-    users = getallusers()
-    for user in users:
-        print(user)
