55db7051f62706009e05f3d119f4b57a383e8ac6
Noha Shehab <nohashehab.iti@gmail.com>
2022-10-09 18:14:21 +0200

    menu circularfix

---
+++ a.py
@@ -0,0 +1,9 @@
+
+def login():
+    print("----------- login ------------")
+    email = 'noha@gmail.com'
+    askforemail =  input("please enter email: ")
+    if email != askforemail:
+        return "NoTFound"
+
+
---
+++ b.py
@@ -0,0 +1,6 @@
+def register():
+    print("----------- register ------------")
+    email = 'noha@gmail.com'
+    askforemail =  input("please enter email: ")
+    if email == askforemail:
+        return  "Found"
